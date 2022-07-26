bb0ff759b1f405f6f0f3f15d4af72ae0f093a064
Samson Zhang <wwsamson@yahoo.com>
2022-07-26 13:21:20 -0700

    add proportional boxes

--- other.js
+++ other.js
@@ -1,7 +1,7 @@
 const width = 800;
 const margin = 24;
-const singleWidth = (width - (3 * margin)) / 4;
-const squaresPerRow = 8;
+const singleWidth = (width - (5 * margin)) / 4;
+const squaresPerRow = 5;
 const squareMargin = 4;
 const squareWidth = singleWidth / squaresPerRow - squareMargin;
 const labelHeight = 48;
@@ -20,7 +20,7 @@ const judgeType = {
     "Loeb": "Final judge",
 }
 
-function render(highlight) {
+function render(highlight, boxPercent) {
     const svg = d3.select("svg");
 
     d3.json("./judges.json").then(data => {
@@ -33,13 +33,13 @@ function render(highlight) {
             .style("display", "none");
 
         const containers = svg
-            .attr("viewBox", `0 0 ${width} 200`)
+            .attr("viewBox", `0 0 ${width} 300`)
             .style("width", "100%")
             .selectAll("g.awardContainer")
             .data(Object.keys(labels))
             .enter()
             .append("g")
-            .style("transform", (d, i) => `translateX(${i * (singleWidth + margin)}px)`);
+            .style("transform", (d, i) => `translateX(${margin + i * (singleWidth + margin)}px)`);
 
         containers.append("text")
             .attr("dominant-baseline", "hanging")
@@ -60,7 +60,7 @@ function render(highlight) {
             .attr("rx", 4)
             .attr("x", (d, i) => (i % squaresPerRow) * singleWidth / squaresPerRow)
             .attr("y", (d, i) => Math.floor(i / squaresPerRow) * (singleWidth / squaresPerRow) + labelHeight)
-            .attr("fill", d => highlight.includes(d.name) ? "red" : "black")
+            .attr("fill", d => highlight.includes(d.name) ? "#0062F1" : "#bbb")
             .on("mouseover", function(event, d) {
                 d3.select(this).attr("fill", "gray");
 
@@ -76,8 +76,36 @@ function render(highlight) {
                     .style("top", event.pageY + 8 + "px");
             })
             .on("mouseout", function(e, d) {
-                d3.select(this).attr("fill", highlight.includes(d.name) ? "red" : "black");
+                d3.select(this).attr("fill", highlight.includes(d.name) ? "#0062F1" : "#bbb");
                 tooltip.style("display", "none");
             });;
+
+        containers.append("rect")
+            .attr("width", d => boxPercent * data.filter(x => x.award === d).length * singleWidth / squaresPerRow)
+            .attr("height", squareWidth + 2)
+            .attr("x", -1)
+            .attr("y", labelHeight - 1)
+            .attr("stroke", "#a13b58")
+            .style("opacity", 0.5)
+            // .attr("stroke-dasharray", "3 3")
+            .attr("stroke-width", 3)
+            .attr("fill", "transparent")
+            .attr("pointer-events", "none");
+
+        svg.append("rect")
+            .attr("width", squareWidth - 2)
+            .attr("height", squareWidth - 2)
+            .attr("stroke", "#a13b58")
+            .style("opacity", 0.5)
+            .attr("stroke-width", 3)
+            .attr("fill", "transparent")
+            .attr("x", margin)
+            .attr("y", 216);
+
+        svg.append("text")
+            .text(`If proportional to US population (${d3.format(".0%")(boxPercent)})`)
+            .attr("dominant-baseline", "middle")
+            .attr("x", margin + squareWidth + 16)
+            .attr("y", 216 + (squareWidth - 2) / 2);
     });
 }
